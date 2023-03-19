e6fdc3c8f8e853067539f544dff2cb1568e4280a
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-03-19 17:05:59 +0100

    Found bug solution to radioBtns not rendering - fixed for suits

--- src/components/DeclareCardRadios.js
+++ src/components/DeclareCardRadios.js
@@ -7,21 +7,23 @@ function DeclareCardRadios({btnValues, groupName, setClaimedCardSuitAndValue, su
     function createRadioBtnGroup(btnV, groupN){
         let radioBtnGroup = [];
         btnV.forEach((value) => {
-            radioBtnGroup.push(["Hello"]);
+            radioBtnGroup.push(
+                <div className="radio-btn">
+                <input
+                    type="radio"
+                    id={value}
+                    value={value}
+                    name={groupN}
+                    defaultChecked={isDefaultValue(value)}
+                    onChange={() => setClaimedCardSuitAndValue(value)}/>
+                <label htmlFor={value}>{value}</label>
+            </div>
+            );
         });
         return radioBtnGroup;
     }
 
-/*     <div className="radio-btn">
-    <input
-        type="radio"
-        id={value}
-        value={value}
-        name={groupN}
-        defaultChecked={isDefaultValue(value)}
-        onChange={() => setClaimedCardSuitAndValue(value)}/>
-    <label htmlFor={value}>{value}</label>
-    </div> */
+
 
     function isDefaultValue(radioBtnValue){
         if(radioBtnValue === suitActual || radioBtnValue === valueActual)
@@ -32,9 +34,22 @@ function DeclareCardRadios({btnValues, groupName, setClaimedCardSuitAndValue, su
             return false;
         }
     }
-    
+
+    // Test remove this stuff
+    let testArr = [<h1>"Hello"</h1>];
+    //testArr.push()
+
+    let buffer = []
+
+    buffer.push(<div>A</div>);
+    buffer.push(<div>B</div>);
+    buffer.push(<div>C</div>);
+
+    // {radioBtnGroupArr}
     return(
-        {radioBtnGroupArr}
+        <>
+            {radioBtnGroupArr}
+        </>
     );
 }
 
