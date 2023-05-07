31df41b50f2744e6bd0741e3e6819de7d2808447
Fredrik Uddsten <12560602+mauritz2@users.noreply.github.com>
2023-05-07 13:36:44 +0200

    Added spades card images

--- src/Constants.js
+++ src/Constants.js
@@ -12,6 +12,20 @@ import HeartsQueen from "./static/hearts_queen.png"
 import HeartsKing from "./static/hearts_king.png"
 import HeartsAce from "./static/hearts_ace.png"
 
+import Spades2 from "./static/spades_2.png"
+import Spades3 from "./static/spades_3.png"
+import Spades4 from "./static/spades_4.png"
+import Spades5 from "./static/spades_5.png"
+import Spades6 from "./static/spades_6.png"
+import Spades7 from "./static/spades_7.png"
+import Spades8 from "./static/spades_8.png"
+import Spades9 from "./static/spades_9.png"
+import Spades10 from "./static/spades_10.png"
+import SpadesJack from "./static/spades_jack.png"
+import SpadesQueen from "./static/spades_queen.png"
+import SpadesKing from "./static/spades_king.png"
+import SpadesAce from "./static/spades_ace.png"
+
 const Constants = new function() {
     this.CARD_VALUE_MAP = {
         "2":2,
@@ -29,7 +43,7 @@ const Constants = new function() {
         "ace":14
     };
     this.CARD_VALUES = Object.keys(this.CARD_VALUE_MAP);
-    this.CARD_SUITS = ["diamonds", "spades", "clovers", "hearts"];
+    this.CARD_SUITS = ["diamonds", "spades", "clovers", "Spades"];
     this.CARD_IMAGES = {
         "hearts 2":Hearts2,
         "hearts 3":Hearts3,
@@ -43,7 +57,20 @@ const Constants = new function() {
         "hearts jack":HearstJack,
         "hearts queen":HeartsQueen,
         "hearts king":HeartsKing,
-        "hearts ace":HeartsAce
+        "hearts ace":HeartsAce,
+        "spades 2":Spades2,
+        "spades 3":Spades3,
+        "spades 4":Spades4,
+        "spades 5":Spades5,
+        "spades 6":Spades6,
+        "spades 7":Spades7,
+        "spades 8":Spades8,
+        "spades 9":Spades9,
+        "spades 10":Spades10,
+        "spades jack":SpadesJack,
+        "spades queen":SpadesQueen,
+        "spades king":SpadesKing,
+        "spades ace":SpadesAce,
     }
   }
   export default Constants;
